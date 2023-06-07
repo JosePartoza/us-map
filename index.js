bade95d11006156fed3ba5edce1334d98bedc2bb
JosePartoza <68497243+JosePartoza@users.noreply.github.com>
2023-06-07 21:48:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const paths = document.querySelectorAll("svg path[data-name]");
+
+paths.forEach((path) => {
+  const stateName = path.getAttribute("data-name");
+  const textElement = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "text"
+  );
+
+  // Get the bounding box of the path
+  const bbox = path.getBBox();
+
+  // Calculate the centroid of the path
+  const centroidX = bbox.x + bbox.width / 2;
+  const centroidY = bbox.y + bbox.height / 2;
+
+  textElement.textContent = stateName;
+  textElement.setAttribute("x", centroidX);
+  textElement.setAttribute("y", centroidY);
+  textElement.setAttribute("text-anchor", "middle");
+  textElement.setAttribute("alignment-baseline", "middle");
+  path.parentNode.appendChild(textElement);
+});
